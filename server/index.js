1fc95d3ad62357985ef72cdff9de15613470a6dd
Md Pear E Habib <habib.austcse31@gmail.com>
2021-10-05 07:43:47 +0600

    nodemon install

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const app=express();
 const cors=require("cors");
-
+const pool=require("./db");
 
 
 //middleware
@@ -12,7 +12,19 @@ app.use(express.json());  //req.body
 //routes
 
 //create a todo
+app.post("/todos",(req,res)=>{
+    try{
+        const {description} =req.body;
+        const newTodo = pool.query(
+            "INSERT INTO todo(description) VALUES($1) RETURNING *",
+            [description]
+        );
+        res.json(newTodo.rows[0]);
 
+    }catch(err){
+        console.error(err.message);
+    }
+})
 
 
 //get all todos
